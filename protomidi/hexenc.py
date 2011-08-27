f420433b986cc05b63834e513ecb6182a5cebaed
Ole Martin Bjørndalen <ombdalen@gmail.com>
2011-08-27 19:55:50 +0200

    Finished the hex encoder.

--- protomidi/hexenc.py
+++ protomidi/hexenc.py
@@ -1,73 +1,89 @@
 #!/usr/bin/env python
 """
-# This is a comment
-9f 8f d9 _H _e _l _l _o __ _w _o _r _l _d d9 c9
-
-' ' => '_'
-'0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' => 'A'
-all others:     d9
-Each byte is converted to three bytes.
-"""
 
 from __future__ import print_function, unicode_literals
 import sys
 import string
 
-alphanum = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-legalchars = alphanum + '_'
 hexchars = '0123456789abcdefABCDEF'
-displaychars = string.digits + string.uppercase + string.lowercase + ' '
+"""
 
-def encode(data, align=True):
-    """
-    Encode data as 
-    """
-    data = bytearray(data)
-    ret = ''
+import sys
 
-    width = 0
+
+class HexEncoder:
+    """
+    Wraps around a filelike object and convert each byte to a three
+    character sequence: two hex chars encoding the byte and a whitespace
+    character to delimit it from the next byte.
     
-    for byte in data:
-        c = chr(byte)        
-        if c in displaychars:
-            if c == ' ':
-                c = '_'  # Escape space as underscore
+    After writing 'wraplen' bytes, the encoder will output a newline
+    instead of a space after the hex byte. (Todo: allow the user to
+    force a newline?)
 
-            if align:
-                ret += '_'  # A leading underscore for alignment
-            
-            ret += c       # Print the character directly
-        else:
-            # Hex encode
-            ret += ('%02x' % byte)
+    Todo: if wraplen == None, wrapping will not happen.
 
-        if width > 70:
-            ret += '\n'
-            width = 0
-        else:
-            ret += ' '
+    Todo: create a StringIO object if file==None? This would be convenient.
 
-            width += 3
+    Todo: the encode can also be used like this:
 
-    return ret
+           hexenc = HexEncoder()
+           for i in range(10):
+               outfile.write(hexenc.encode(i))
 
-def decode(text):
-    bytes = bytearray()
+       or even:
 
-    while text:
-        chrs = text[:3]
-        text = text[3:]
+           enc = HexEncoder()
+    
+           for i in range(10):
+               outfile.write(enc(i))
+    """
 
-        if chrs[0] == '.':
-            bytes.append(orc(chrs[1]))
-        else:
-            bytes.append(int(chrs[:2], 16))
+    def __init__(self, file=file, wraplen=None):
+        """
+        Create a new HexEncoder and wrap it around the filelike object
+        'file'.
+        """
+
+        self.file = file
+        self.wraplen = wraplen
+
+
+    def write(self, data):
+
+        for byte in data:
+            byte = ord(byte)
+            byte = self.encode(byte)
+
+            self.file.write(byte)
+
+            if self.wraplen:
+                self.file.write('\n')
+                self.pos = 0
+            else:
+                self.file.write(' ')
+
+    def encode(self, byte):
+        """
+        Encode one byte and return it. They byte
+        must be an integer in the range [0...255].
+        """
+        assert isinstance(byte, int)
+        assert 0 <= byte < 255
+
+        return ('%02x' % byte)
+
+    __call__ = encode  # Convenient shortcut
+
+    def flush(self):
+        """
+        Flush the output file.
+        """
+
+        self.out.flush()
+
 
 if __name__ == '__main__':
-    if '--align' in sys.argv:
-        align = True
-    else:
-        align = False
-
-    data = sys.stdin.read()
-    print(encode(data, align=align))
+    enc = HexEncoder(sys.stdout)
+    
+    print(' '.join([enc(i) for i in range(10)]))
