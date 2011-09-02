0110d28e24d4bf2b51edc0ee6fa2dc9dfc3c7903
Ole Martin Bjørndalen <ombdalen@gmail.com>
2011-09-02 05:02:08 +0200

    Cleaned up test.py

--- test.py
+++ test.py
@@ -4,23 +4,23 @@ import protomidi
 from protomidi.msg import *
 from protomidi.io import Input, Output, context, get_devinfo_dicts
 
+portmidi = context
+
 import time
 
 outputs = protomidi.io.get_output_map()
 pprint.pprint(outputs)
 
-with context():
-
-
+with portmidi():
     out = Output(0)
 
     while 1:
-        # Send random programchange
         out.send(program_change(number=random.randrange(128)))
 
         note = random.randrange(128)
-        out.send(note_on(note=note, velocity=100))
 
+        out.send(note_on(note=note, velocity=100))
         time.sleep(0.1)
+
         out.send(note_off(note=note, velocity=100))
         time.sleep(0.1)
