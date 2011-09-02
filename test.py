3842ecd1cb37f050b249ca61ea867d4fdcea4900
Ole Martin Bjørndalen <ombdalen@gmail.com>
2011-09-02 04:33:12 +0200

    Renamed portmidi => io.

--- test.py
+++ test.py
@@ -1,22 +1,24 @@
 import random
 import protomidi
-from protomidi import portmidi
 from protomidi.msg import *
+from protomidi.io import Input, Output, context, get_devinfo_dicts
 
 import time
 
+with context():
 
-with portmidi.context():
-    devices = portmidi.get_devinfo_dicts()
+    devices = get_devinfo_dicts()
+    # print(devices)
 
-    # print(portmidi.get_defoutput())
-
-    out = portmidi.Output(2)
+    out = Output(2)
 
     while 1:
+        # Send random programchange
         out.send(program(number=random.randrange(128)))
+
         note = random.randrange(128)
         out.send(note_on(note=note, velocity=100))
+
         time.sleep(0.1)
         out.send(note_off(note=note, velocity=100))
         time.sleep(0.1)
