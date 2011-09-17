e07f16d7e15dba1492dd8b95f020e067da6a77ae
Ole Martin Bjørndalen <ombdalen@gmail.com>
2011-09-18 01:34:41 +0200

    segfault.py segfaults the library somehow. Need to debug this.

---
+++ segfault.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+"""
+This segfaults. Start Midi Keys, then run this script with 'python -i', wait for 2 seconds and
+play something on Midi Keys. Boom, segfault.
+"""
+
+from __future__ import print_function
+
+import time
+import random
+import pprint
+import traceback
+import protomidi
+from protomidi.msg import *
+import protomidi.portmidi as pm
+
+pm.debug = True
+
+pprint.pprint(pm.get_devinfo())
+
+pm.initialize()
+try:
+    out = pm.Output()
+except pm.Error:
+    traceback.print_exc()
+
+print('Sleeping for 2 seconds before opening input')
+time.sleep(2)
+print('Opening input')
+input = pm.Input()
+print('Please play something on Midi Keys now')
+
+"""
+while 1:
+    note = random.randrange(128)
+
+    out.send(note_on(note=note, velocity=75))
+    time.sleep(0.25)
+    out.send(note_off(note=note))
+"""
