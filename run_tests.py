61bea4fa72103aba57e308a04f47452c06354e07
Ole Martin Bjørndalen <ombdalen@gmail.com>
2013-06-26 21:42:10 +0200

    Reformatted and rewrote docstrings for test suite.

--- run_tests.py
+++ run_tests.py
@@ -5,18 +5,14 @@ import mido
 
 class TestMessages(unittest.TestCase):
     def test_msg_equality(self):
-        """
-        Two messages created with same parameters should be equal.
-        """
+        """Two messages created with same parameters should be equal."""
         msg1 = mido.new('note_on', channel=1, note=2, velocity=3)
         msg2 = mido.new('note_on', channel=1, note=2, velocity=3)
 
         self.assertEqual(msg1, msg2)
 
     def test_pitchwheel(self):
-        """
-        Check if pitchwheel type check and encoding is working.
-        """
+        """Check if pitchwheel type check and encoding is working."""
         msg = mido.new('pitchwheel', value=mido.msg.PITCHWHEEL_MIN)
         bytes = msg.bytes()
         self.assertTrue(bytes[1] == bytes[2] == 0)
@@ -26,23 +22,14 @@ class TestMessages(unittest.TestCase):
         self.assertTrue(bytes[1] == bytes[2] == 127)
 
     def test_pitchwheel_encode_parse(self):
-        """
-        Check if pitchwheel with maximal value encodes correctly.
-        """
+        """Encode and parse pitchwheel with value=0."""
         msg1 = mido.new('pitchwheel', value=0)
         msg2 = mido.parse(msg1.bytes())
         
         self.assertEqual(msg1, msg2)
 
     def test_channel_value(self):
-        """
-        Message.__init__() 
-
-        - if nothing is passed, the value is 0
-        - if first argument is a status_byte,
-          channel is the lower 4 bits of the status_byte
-        - the channel keyword argument overrides this value
-        """
+        """See if the channel masking and overrides work in init."""
 
         self.assertEqual(mido.new('note_on').channel, 0)
         self.assertEqual(mido.new('note_on', channel=1).channel, 1)
@@ -54,42 +41,34 @@ class TestMessages(unittest.TestCase):
 class TestParser(unittest.TestCase):
     
     def test_parse(self):
-        """
-        Parse a note_on msg.
-        """
+        """Parse a note_on msg and compare it to one created with new()."""
         parsed = mido.parse(b'\x90\x4c\x20')
         other = mido.new('note_on', channel=0, note=76, velocity=32)
         self.assertEqual(parsed, other)
 
     def test_parse_stray_data(self):
-        """
-        Stray data bytes (not inside a message) should be ignored.
-        """
+        """The parser should ignore stray data bytes."""
         ret = mido.parseall(b'\x20\x30')
         
         self.assertEqual(ret, [])
 
     def test_parse_stray_status_bytes(self):
-        """
-        Stray status bytes (status byte followed by too few data bytes)
-        should be ignored.
-        """
+        """The parser should ignore stray status bytes."""
         ret = mido.parseall(b'\x90\x90\xf0')
         
         self.assertEqual(ret, [])
 
     def test_encode_and_parse(self):
-        """
-        Encode a message and parse it. Should return the same message.
+        """Encode a message and then parse it.
+
+        Should return the same message.
         """
         msg1 = mido.new('note_on')
         msg2 = mido.parse(msg1.bytes())
         self.assertEqual(msg1, msg2)
 
     def test_put_byte(self):
-        """
-        Encode a message and parse it. Should return the same message.
-        """
+        """Put various things into put_byte()."""
         import mido.parser
 
         p = mido.parser.Parser()
@@ -99,7 +78,7 @@ class TestParser(unittest.TestCase):
 
         self.assertRaises(TypeError, p.put_byte, [1, 2, 3])
         self.assertRaises(ValueError, p.put_byte, -1)
-        self.assertRaises(ValueError, p.put_byte, 256)
+        self.assertRaises(ValueError, p.put_byte, 256)    
 
     # Todo: Parser should not crash when parsing random data
     #def test_parse_random_bytes(self):
