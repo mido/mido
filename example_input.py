2f9e3171c81b573e66f9aeade5da88c374eb592e
Ole Martin Bjørndalen <ombdalen@gmail.com>
2013-06-27 07:14:47 +0200

    Updated example.

--- example_input.py
+++ example_input.py
@@ -6,8 +6,8 @@ will return how many messages you can safely read without blocking, so
 you can implement nonblocking read with:
 
     while 1:
-        while port.poll():
-            message = input.receive()
+        for _ in range(port.poll()):
+            message = port.receive()
             print(message)
 
         # ... do something else
