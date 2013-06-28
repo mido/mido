513fb6f0557253a87eb19032743d631f089f8cdf
Ole Martin Bjørndalen <ombdalen@gmail.com>
2013-06-28 23:14:06 +0200

    Wrote a test for sysex parse / format. It tends to break. Also made other test more readable.

--- run_tests.py
+++ run_tests.py
@@ -10,7 +10,7 @@ class TestMessages(unittest.TestCase):
         msg1 = mido.new('note_on', channel=1, note=2, velocity=3)
         msg2 = mido.new('note_on', channel=1, note=2, velocity=3)
 
-        self.assertEqual(msg1, msg2)
+        self.assertTrue(msg1 == msg2)
 
     def test_pitchwheel(self):
         """Check if pitchwheel type check and encoding is working."""
@@ -27,17 +27,22 @@ class TestMessages(unittest.TestCase):
         msg1 = mido.new('pitchwheel', pitch=0)
         msg2 = mido.parse(msg1.bytes())
         
-        self.assertEqual(msg1, msg2)
+        self.assertTrue(msg1 == msg2)
 
     def test_channel_value(self):
         """See if the channel masking and overrides work in init."""
 
-        self.assertEqual(mido.new('note_on').channel, 0)
-        self.assertEqual(mido.new('note_on', channel=1).channel, 1)
+        self.assertTrue(mido.new('note_on').channel == 0)
+        self.assertTrue(mido.new('note_on', channel=1).channel == 1)
 
-        self.assertEqual(mido.new(0x90).channel, 0)
-        self.assertEqual(mido.new(0x91).channel, 1)
-        self.assertEqual(mido.new(0x90, channel=1).channel, 1)
+        self.assertTrue(mido.new(0x90).channel == 0)
+        self.assertTrue(mido.new(0x91).channel == 1)
+        self.assertTrue(mido.new(0x90, channel=1).channel == 1)
+
+    def test_sysex(self):
+        original = mido.new('sysex', data=(1, 2, 3, 4, 5))
+        parsed = mido.parse(original.bytes())
+        self.assertTrue(original == parsed)
 
 class TestParser(unittest.TestCase):
     
@@ -45,19 +50,19 @@ class TestParser(unittest.TestCase):
         """Parse a note_on msg and compare it to one created with new()."""
         parsed = mido.parse(b'\x90\x4c\x20')
         other = mido.new('note_on', channel=0, note=76, velocity=32)
-        self.assertEqual(parsed, other)
+        self.assertTrue(parsed == other)
 
     def test_parse_stray_data(self):
         """The parser should ignore stray data bytes."""
         ret = mido.parse_all(b'\x20\x30')
         
-        self.assertEqual(ret, [])
+        self.assertTrue(ret == [])
 
     def test_parse_stray_status_bytes(self):
         """The parser should ignore stray status bytes."""
         ret = mido.parse_all(b'\x90\x90\xf0')
         
-        self.assertEqual(ret, [])
+        self.assertTrue(ret == [])
 
     def test_encode_and_parse(self):
         """Encode a message and then parse it.
@@ -66,7 +71,7 @@ class TestParser(unittest.TestCase):
         """
         msg1 = mido.new('note_on')
         msg2 = mido.parse(msg1.bytes())
-        self.assertEqual(msg1, msg2)
+        self.assertTrue(msg1 == msg2)
 
     def test_feed_byte(self):
         """Put various things into feed_byte()."""
