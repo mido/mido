5d739b541175e2dc85ce9865312899fe1f64b6ee
Ole Martin Bjørndalen <ombdalen@gmail.com>
2013-07-03 23:04:46 +0200

    Time is not included in str(message).

--- mido/messages.py
+++ mido/messages.py
@@ -384,7 +384,7 @@ class Message(object):
         return '<{} message {}>'.format(self.type, ', '.join(parts))
 
     def __str__(self):
-        return _format_as_string(self, include_time=True)
+        return _format_as_string(self, include_time=False)
 
     def __eq__(self, other):
         """Compare message to another for equality.
