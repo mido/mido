e8cfe3c50f80a548a089f9318d5408e1516516ba
Ole Martin Bjørndalen <ombdalen@gmail.com>
2013-07-20 14:23:51 +0200

    Trying 1.0.0 release again.

--- setup.py
+++ setup.py
@@ -36,8 +36,7 @@ setup(
     # install_requires=required,  # Unknown option in Python 3
     license='MIT',
     classifiers=(
-        # 'Development Status :: 5 - Production/Stable',
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
