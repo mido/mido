04b6e64ce68e89833f43ead1272b91d30b53f128
Ole Martin Bjørndalen <ombdalen@gmail.com>
2013-07-27 14:29:32 +0200

    Added example buffer_port.py.

---
+++ examples/buffer_port.py
@@ -0,0 +1,26 @@
+from mido import Message
+from mido.ports import BaseInput, BaseOutput
+
+class BufferPort(BaseInput, BaseOutput):
+    def _send(self, message):
+        self._messages.append(message)
+
+    def _pending(self):
+        return len(self._messages)
+    
+    def _get_device_type(self):
+        return 'buffer'
+
+    # Make sure we never need to wait.
+    __iter__ = BaseInput.iter_pending
+
+if __name__ == '__main__':
+    buffer = BufferPort()
+
+    buffer.send(Message('program_change', program=7))
+    buffer.send(Message('control_change', control=1, value=2))
+    for message in buffer:
+        print(message)
+
+    buffer.send(Message('control_change', control=2, value=3))
+    print list(buffer)
