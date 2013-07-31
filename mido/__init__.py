b260843cef6f58edc1e63b389e27b63eb8d3b3fe
Ole Martin Bjørndalen <ombdalen@gmail.com>
2013-07-31 18:20:54 +0200

    Bumped version to 1.0.1.

--- mido/__init__.py
+++ mido/__init__.py
@@ -84,7 +84,7 @@ __author__ = 'Ole Martin Bjørndalen'
 __email__ = 'ombdalen@gmail.com'
 __url__ = 'http://mido.readthedocs.org/'
 __license__ = 'MIT'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 # Prevent splat import.
 __all__ = []
