ffee496465a53841a0ab3442612c97e0c4c89217
Ole Martin Bjørndalen <ombdalen@gmail.com>
2013-08-07 02:18:03 +0200

    Rewrote the backend system with a Backend class.

--- mido/__init__.py
+++ mido/__init__.py
@@ -91,77 +91,110 @@ __version__ = '1.0.0'
 # Prevent splat import.
 __all__ = []
 
-def get_backend_name():
-    """Return name of the current backend."""
-    return os.environ.get('MIDO_BACKEND', 'mido.backends.portmidi')    
+class Backend(object):
+    def __init__(self, path, on_demand=False, use_environ=False):
+        self.path = path
+        self.use_environ = use_environ
 
-def _get_backend(name=None):
-    if name is None:
-        name = get_backend_name()
-    return importlib.import_module(name)
+        self.module = None
+        if not on_demand:
+            self._import()
 
-def open_input(name=None, **kwargs):
-    """Open an input port.
+    def _import(self):
+        if self.module is None:
+            self.module = importlib.import_module(self.path)
 
-    If the environment variable MIDO_DEFAULT_INPUT is set,
-    if will override the default port.
-    """
-    if name is None:
-        name = os.environ.get('MIDO_DEFAULT_INPUT', None)
-    return _get_backend().Input(name, **kwargs)
-
-def open_output(name=None, **kwargs):
-    """Open an output port.
-
-    If the environment variable MIDO_DEFAULT_OUTPUT is set,
-    if will override the default port.
-    """
-    if name is None:
-        name = os.environ.get('MIDO_DEFAULT_OUTPUT')
-    return _get_backend().Output(name, **kwargs)
-
-def open_ioport(name=None, **kwargs):
-    """Open a port for input and output.
-
-    If the environment variable MIDO_DEFAULT_IOPORT is set,
-    if will override the default port.
-    """
-    if name is None:
-        name = os.environ.get('MIDO_DEFAULT_IOPORT')
-    backend = _get_backend()
-    if hasattr(backend, 'IOPort'):
-        if name is None:
-            name = os.environ.get('MIDO_DEFAULT_IOPORT')
-        return backend.IOPort(name, **kwargs)
-    else:
-        if name is None:
-            # MIDO_DEFAULT_IOPORT overrides the other two variables.
-            name = os.environ.get('MIDO_DEFAULT_IOPORT')
-            if name is not None:
-                input_name = output_name = name
-            else:
-                input_name = os.environ.get('MIDO_DEFAULT_INPUT')
-                output_name = os.environ.get('MIDO_DEFAULT_OUTPUT')
+    def _env(self, name):
+        if self.use_environ:
+            return os.environ.get(name)
         else:
-            input_name = output_name = name
+            return None
 
-        return ports.IOPort(backend.Input(input_name, **kwargs),
-                            backend.Output(output_name, **kwargs))
+    def open_input(self, name=None, **kwargs):
+        """Open an input port.
 
-def get_input_names():
-    """Return a sorted list of all input port names."""
-    devices = _get_backend().get_devices()
-    names = [device['name'] for device in devices if device['is_input']]
-    return list(sorted(names))
+        If the environment variable MIDO_DEFAULT_INPUT is set,
+        if will override the default port.
+        """
+        self._import()
+        if name is None:
+            name = self._env('MIDO_DEFAULT_INPUT')
+        return self.module.Input(name, **kwargs)
+
+    def open_output(self, name=None, **kwargs):
+        """Open an output port.
+        
+        If the environment variable MIDO_DEFAULT_OUTPUT is set,
+        if will override the default port.
+        """
+        self._import()
+        if name is None:
+            name = self._env('MIDO_DEFAULT_OUTPUT')
+        return self.module.Output(name, **kwargs)
+
+    def open_ioport(self, name=None, **kwargs):
+        """Open a port for input and output.
+
+        If the environment variable MIDO_DEFAULT_IOPORT is set,
+        if will override the default port.
+        """
+        self._import()
+
+        if name is None:
+            name = self._env('MIDO_DEFAULT_IOPORT')
+        if hasattr(self.module, 'IOPort'):
+            if name is None:
+                name = self._env('MIDO_DEFAULT_IOPORT')
+            return self.module.IOPort(name, **kwargs)
+        else:
+            if name is None:
+                # MIDO_DEFAULT_IOPORT overrides the other two variables.
+                name = self._env('MIDO_DEFAULT_IOPORT')
+                if name is not None:
+                    input_name = output_name = name
+                else:
+                    input_name = self._env('MIDO_DEFAULT_INPUT')
+                    output_name = self._env('MIDO_DEFAULT_OUTPUT')
+            else:
+                input_name = output_name = name
+
+            return ports.IOPort(self.module.Input(input_name, **kwargs),
+                                self.module.Output(output_name, **kwargs))
+
+    def get_input_names(self):
+        """Return a sorted list of all input port names."""
+        self._import()
+        devices = self.module.get_devices()
+        names = [device['name'] for device in devices if device['is_input']]
+        return list(sorted(names))
+
+    def get_output_names(self):
+        """Return a sorted list of all output port names."""
+        self._import()
+        devices = self.module.get_devices()
+        names = [device['name'] for device in devices if device['is_input']]
+        return list(sorted(names))
+
+    def get_ioport_names(self):
+        """Return a sorted list of all I/O port names."""
+        self._import()
+        return sorted(
+            set(self.get_input_names()) & set(self.get_output_names()))        
 
 
-def get_output_names():
-    """Return a sorted list of all input port names."""
-    devices = _get_backend().get_devices()
-    names = [device['name'] for device in devices if device['is_output']]
-    return list(sorted(names))
+def set_backend(path):
+    """Set current backend.
+
+    This will replace all the open_*() and get_*_name() functions
+    in top level mido module. The module will be loaded the first
+    time one of those functions is called."""
+
+    glob = globals()
+
+    backend = glob['backend'] = Backend(path, use_environ=True)
+    for name in dir(backend):
+        if name.split('_')[0] in ['open', 'get']:
+            glob[name] = getattr(backend, name)
 
 
-def get_ioport_names():
-    """Return the names of all ports that allow input and output."""
-    return sorted(set(get_input_names()) & set(get_output_names()))
+set_backend(os.environ.get('MIDO_BACKEND', 'mido.backends.portmidi'))
