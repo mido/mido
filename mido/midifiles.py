bbc135618d35572452744c7810a7fb3411817683
Rapolas Binkys <r.binkyswps@gmail.com>
2013-08-18 16:56:30 -0400

    Fixed error with returning unicode string as well as replaced instances of System.out.write with print statements.

--- mido/midifiles.py
+++ mido/midifiles.py
@@ -154,7 +154,7 @@ class MidiTrack(list):
             if message.type == 'track_name':
                 return message.name
         else:
-            return 'u'
+            return u''
 
     def _set_name(self, name):
         # Find the first track_name message and modify it.
@@ -518,10 +518,10 @@ class MidiFile:
         MidiFile.print_tracks(showOnlyMetaMessages) -> will print only MetaMessages
         """
         for i, track in enumerate(self.tracks):
-            sys.stdout.write('=== Track {}\n'.format(i))
+            print('=== Track {}\n'.format(i))
             for message in track:
                 if not isinstance(message, MetaMessage) and showOnlyMetaMessages:
                     pass
                 else:
-                    sys.stdout.write('  {!r}\n'.format(message))
+                    print('  {!r}\n'.format(message))
                 
