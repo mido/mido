99fc8ec6bf7290741a233fcd7c8e72e745400f94
Ole Martin Bjørndalen <ombdalen@gmail.com>
2013-10-14 14:21:29 +0200

    Rewrote send.py example.

--- examples/ports/send.py
+++ examples/ports/send.py
@@ -18,18 +18,18 @@ else:
 # A pentatonic scale
 notes = [60, 62, 64, 67, 69, 72]
 
+def send(message, sleep_time):
+    print('Sending {}'.format(message))
+    port.send(message)
+    time.sleep(sleep_time)
+
+on = mido.Message('note_on')
+off = mido.Message('note_off')
+
 with mido.open_output(portname, autoreset=True) as port:
     print('Using {}'.format(port))
     while 1:
-        note = random.choice(notes)
-
-        message = mido.Message('note_on', note=note)
-        print('Sending {}'.format(message))
-        port.send(message)
-        time.sleep(0.05)
-
-        message = mido.Message('note_off', note=note)
-        print('Sending {}'.format(message))
-        port.send(message)
-        time.sleep(0.1)
+        on.note = off.note = random.choice(notes)
+        send(on, 0.05)
+        send(off, 0.1)
 print()
