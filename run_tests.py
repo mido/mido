df8de2fcbc0314b7f9e9a3bfd494e2c10473f4d7
Ole Martin Bjørndalen <ombdalen@gmail.com>
2014-08-03 10:57:29 +0200

    Copied new version of run_tests.py from dbfread.

--- run_tests.py
+++ run_tests.py
@@ -2,47 +2,60 @@
 """
 Run unit tests in Python 2 and 3.
 """
+from __future__ import print_function
 import re
 import sys
 from subprocess import Popen, PIPE
 
-requirements = {2: (2, 7, 0),
-                3: (3, 2, 0)}
+def get_version(program):
+    try:
+        proc = Popen([program, '--version'], stdout=PIPE, stderr=PIPE)
+        line = proc.stdout.read().decode('utf-8')
+        line += proc.stderr.read().decode('utf-8')
+        proc.wait()
+    except OSError:
+        return None
 
-def get_pythons():
-    pythons = {2: None,
-               3: None}
+    # The line contains "Python 2.7.4".
+    # Get the version number as a tuple of ints.
+    match = re.search('Python (\d+)\.(\d+)\.(\d+)', line)
+    if match:
+        return tuple(map(int, match.groups()))
+    else:
+        return None
 
-    """Locates Pyhton 2 and 3 and returns them as a tuple."""
-    for major_version, requirement in requirements.items():
-        for python in ['python', 'python2', 'python3']:
-            try:
-                proc = Popen([python, '--version'], stdout=PIPE, stderr=PIPE)
-                line = proc.stdout.read().decode('utf-8')
-                line += proc.stderr.read().decode('utf-8')
-                proc.wait()
-            except OSError:
-                # Program was not found.
-                continue
 
-            # The line contains "Python 2.7.4".
-            # Get the version number as a tuple of ints.
-            match = re.search('Python (\d+)\.(\d+)\.(\d+)', line)
-            if match:
-                version = tuple(map(int, match.groups()))
-                if version[0] == major_version and version > requirement:
-                    pythons[major_version] = python
+def get_python(required_version):
+    major = required_version[0]
+    minor = required_version[1]
 
-    for major_version in pythons:
-        if pythons[major_version] is None:
-            sys.exit('Python {} >= {} not found'.format(
-                    major_version,
-                    requirements[major_version]))
+    for program in ['python{}'.format(major), 'python']:
+        version = get_version(program)
+        if version[0] == major and version >= required_version:
+            return program
 
-    return (pythons[2], pythons[3])
+    return None
 
-for python in get_pythons():
-    proc = Popen([python, 'tests.py'])
-    proc.wait()
-    if proc.returncode != 0:
-        sys.exit('Tests failed with {}.'.format(python))
+REQUIRED_VERSIONS = [(2, 7, 0), (3, 2, 0)]
+
+def main():
+    programs = []
+    for version in REQUIRED_VERSIONS:
+        program = get_python(version)
+        if program is None:
+            print('Python {} >= {} not found'.format(version[0],
+                                                     version), file=sys.stderr)
+        else:
+            programs.append(program)
+
+    if len(programs) < len(REQUIRED_VERSIONS):
+        return 1
+
+    for program in programs:
+        proc = Popen([program, 'tests.py'])
+        proc.wait()
+        if proc.returncode != 0:
+            sys.exit('Tests failed with {}.'.format(python))
+
+            
+sys.exit(main())
