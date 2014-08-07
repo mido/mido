dba27ceb24f7e491ccadf0af3bff2f339ac9e4fa
Ole Martin Bjørndalen <ombdalen@gmail.com>
2014-08-07 21:34:59 +0200

    Renamed pre-commit to run_tests.sh.

---
+++ run_tests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Runs tests in Python 2 and 3.
+#
+# If you want tests to run before each commit you can install this as
+# a pre-commit hook:
+#
+#   ln -s ../../run_tests.sh pre-commit
+#
+# The commit will now be canceled if tests fail.
+#
+function display() {
+  echo -e '\e[0;33m' $* '\e[0m'
+}
+
+display Python 2
+py.test || exit 1
+
+display Python 3
+py.test-3 || exit 1
