9e0cf8b305c2aef4af3665c002db7bc59fa4e11d
Ole Martin Bjørndalen <ombdalen@gmail.com>
2015-04-26 12:38:47 +0200

    Improved port tests a bit.

--- mido/test_ports.py
+++ mido/test_ports.py
@@ -2,16 +2,16 @@ from pytest import raises
 from .messages import Message
 from .ports import BaseIOPort
 
-def test_base_ioport():
+def test_ioport():
     class Port(BaseIOPort):
         def _open(self):
-            self.test_value = True
+            self.close_called = False
 
         def _send(self, message):
             self._messages.append(message)
 
         def _close(self):
-            self.test_value = False
+            self.close_called = True
 
     with Port('Name') as port:
         assert port.name == 'Name'
@@ -21,22 +21,34 @@ def test_base_ioport():
 
         with raises(TypeError): port.send('not a message')
 
-        # Send message.
+    with Port('Name') as port:
         message = Message('note_on')
-        port.send(message)
 
-        # Receive a message. (Blocking.)
-        assert isinstance(port.receive(), Message)
+        # Send and receive a message. (Blocking.)
+        #port.send(message)
+        #message2 = port.receive()
+        #raise ValueError(('ost', message, message2))
 
         # Receive a message. (Non-blocking.)
         port.send(message)
-        assert isinstance(port.receive(block=False), Message)
+        message2 = port.receive(block=False)
         assert port.receive(block=False) is None
+
+    with Port('Name') as port:
+        message = Message('note_on')
 
         port.send(message)
         port.send(message)
         assert port.pending() == 2
         assert list(port.iter_pending()) == [message, message]
+
+    with Port('Name') as port:
+        port.close()
+        assert port.close_called
+        port.close_called = False
+        port.close()
+        assert port.closed
+        assert not port.close_called
 
         # Todo: should this type of port close (and/or stop iteration)
         # when there are no messages?
@@ -45,9 +57,7 @@ def test_base_ioport():
         # assert port.pending() == 2
         # assert list(port) == [message, message]
 
-    assert port.closed
-
-def test_non_finite_port():
+def test_close_inside_iteration():
     # This type of port can close when it runs out of messages.
     # (And example of this is socket ports.)
     #
@@ -56,14 +66,21 @@ def test_non_finite_port():
     message = Message('note_on')
 
     class Port(BaseIOPort):
-        def _open(self):
+        def __init__(self, messages):
+            BaseIOPort.__init__(self)
             # Simulate some messages that arrived
             # earlier.
-            self._messages.extend([message, message])
+            self._messages.extend(messages)
+            self.closed = False
 
         def _receive(self, block=True):
             # Oops, the other end hung up.
-            self.close()
+            if self._messages:
+                return self._messages.popleft()
+            else:
+                self.close()
+                return None
 
-    with Port() as port:
+    message = Message('note_on')
+    with Port([message, message]) as port:
         assert len(list(port)) == 2
