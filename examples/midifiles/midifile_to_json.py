2ee9b53c286f06d8745d3f908d32d0b4c2465cc0
Ole Martin Bjørndalen <ombdalen@gmail.com>
2016-10-28 01:31:16 +0200

    Added example midifile_to_json.py.

---
+++ examples/midifiles/midifile_to_json.py
@@ -0,0 +1,21 @@
+import sys
+import json
+import pprint
+import mido
+
+
+def midifile_to_dict(mid):
+    tracks = []
+    for track in mid.tracks:
+        tracks.append([vars(msg).copy() for msg in track])
+
+    return {
+        'ticks_per_beat': mid.ticks_per_beat,
+        'tracks': tracks,
+    }
+
+mid = mido.MidiFile(sys.argv[1])
+
+print(json.dumps(midifile_to_dict(mid), indent=2))
+
+
