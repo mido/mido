566007763c927643277ab4cbc9654f295b91cc61
Ole Martin Bjørndalen <ombdalen@gmail.com>
2016-10-29 22:52:15 +0200

    Updated encode+decode test.

--- mido/messages/test_encode.py
+++ mido/messages/test_encode.py
@@ -5,14 +5,16 @@ from .decode import decode_msg
 def test_encode_decode_all():
     """Encode and then decode all messages on all channels.
 
-    First with all data bytes 0x00, then 0x7f (min and max values).
+    Each data byte is different so that the test will fail if the
+    bytes are swapped during encoding or decoding.
     """
 
-    for data_byte in [0x00, 0x7f]:
-        for status_byte, spec in SPEC_BY_STATUS.items():
-            if status_byte == 0xf0:
-                msg_bytes = [0xf0, data_byte, 0xf7]
-            else:
-                msg_bytes = [status_byte] + [data_byte] * (spec['length'] - 1)
-
-            assert encode_msg(decode_msg(msg_bytes)) == msg_bytes
+    data_bytes = [1, 2, 3]
+    
+    for status_byte, spec in SPEC_BY_STATUS.items():
+        if status_byte == 0xf0:
+            msg_bytes = [0xf0] + data_bytes + [0xf7]
+        else:
+            msg_bytes = [status_byte] + data_bytes[:spec['length'] - 1]
+            
+        assert encode_msg(decode_msg(msg_bytes)) == msg_bytes
