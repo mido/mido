832d712fcebecb004c2dab120efd9c23082dc137
Ole Martin Bjørndalen <ombdalen@gmail.com>
2017-01-28 12:21:40 +0100

    Refactored RtMidi backend again.
    Moved message queue and parsing out into a ParserQueue class. This
    class should be made publicly avaiable once the API is settled.
    
    Split Port into Input and Output classes. There was too much "if
    is_input" and "if is_output" going on.
    
    No longer inherits from BasePort/BaseInput/BaseOutput. Multiple
    inheritance was again causing problems. For example I found that
    self._open() was called twice because BaseInput and BaseOutput both
    call BasePort.__init__() which calls self._open().
    
    This was tried before in commit
    7f752f7b072bb3c25870b3aca8ef26106f311466, but was abandoned because it
    required the port to implement its own __iter__(), iter_pending(),
    panic(), reset() etc.
    
    This commit attempts to solve the problem with mixins. For example:
    
        class Input(PortMethods, InputMethods):
            ...
    
    will equip the port with __enter__(), __exit__(), __repr__(),
    __iter__(), iter_pending(). These metods don't introduce any instance
    variables or modify internal state. All they do is call self.send(),
    self.receive(), self.poll() and self.close().
    
    This looks fairly promising but some more refinement is needed. For
    example the port must still keep track of self.closed and
    self.autoreset and remember to call self.reset() if self.autoreset ==
    True.

---
+++ mido/backends/_common.py
@@ -0,0 +1,169 @@
+"""
+These classes will be made publicly available once their API is settled. For now they should only be used
+inside this package.
+"""
+
+from ..parser import Parser
+from ..py2 import PY2
+
+if PY2:
+    import Queue as queue
+else:
+    import queue
+
+
+class ParserQueue:
+    """
+    Thread safe message queue with built in MIDI parser.
+
+    This should be avaiable to other backend implementations and perhaps
+    also in the public API, but the API needs a bit of review. (Ideally This
+    would replace the parser.)
+
+    q = ParserQueue()
+
+    q.feed([0xf8, 0, 0])
+    q.put(msg)
+
+    msg = q.get()
+    msg = q.poll()
+    for msg in q:
+        ...
+    """
+    def __init__(self):
+        self._queue = queue.Queue()
+        self._parser = Parser()
+
+    def put(self, msg):
+        self._queue.put(msg)
+
+    def feed(self, msg_bytes):
+        # Todo: should this be protected somehow?
+        # No, it's better to put a lock around reading AND parsing.
+        self._parser.feed(msg_bytes)
+        for msg in self._parser:
+            self.put(msg)
+
+    def _get_py2(self):
+        # In Python 2 queue.get() doesn't respond to CTRL-C. A workaroud is
+        # to call queue.get(timeout=100) (very high timeout) in a loop, but all
+        # that does is poll with a timeout of 50 milliseconds. This results in
+        # much too high latency.
+        #
+        # It's better to do our own polling with a shorter sleep time.
+        #
+        # See Issue #49 and https://bugs.python.org/issue8844
+        sleep_time = ports.get_sleep_time()
+        while True:
+            try:
+                return self._queue.get_nowait()
+            except queue.Empty:
+                if block:
+                    time.sleep(sleep_time)
+                    continue
+                else:
+                    return None
+
+    # Todo: add timeout?
+    def get(self):
+        if PY2:
+            return self._get_py2()
+        else:
+            return self._queue.get()
+
+    def poll(self):
+        try:
+            return self._queue.get_nowait()
+        except queue.Empty:
+            return None
+
+    def iterpoll(self):
+        while True:
+            msg = self.poll()
+            if msg:
+                yield msg
+            else:
+                return
+
+
+class PortMethods(object):
+    def __del__(self):
+        self.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.close()
+        return False
+
+    def __repr__(self):
+        if self.closed:
+            state = 'closed'
+        else:
+            state = 'open'
+
+        capabilities = self.is_input, self.is_output
+        port_type = {
+            (True, False): 'input',
+            (False, True): 'output',
+            (True, True): 'I/O port',
+            (False, False): 'mute port',
+            }[capabilities]
+
+        name = self.name or ''
+
+        try:
+            device_type = self._device_type
+        except AttributeError:
+            device_type = self.__class__.__name__
+
+        return '<{} {} {!r} ({})>'.format(
+            state, port_type, name, device_type)
+
+
+class InputMethods(object):
+
+    def iter_pending(self):
+        """Iterate through pending messages."""
+        while True:
+            msg = self.poll()
+            if msg is None:
+                return
+            else:
+                yield msg
+
+    def __iter__(self):
+        """Iterate through messages until the port closes."""
+        # This could have simply called receive() in a loop, but that
+        # could result in a "port closed during receive()" error which
+        # is hard to catch here.
+        self._check_callback()
+        while True:
+            try:
+                yield self.receive()
+            except IOError:
+                if self.closed:
+                    # The port closed before or inside receive().
+                    # (This makes the assumption that this is the reason,
+                    # at the risk of masking other errors.)
+                    return
+                else:
+                    raise
+
+
+class OutputMethods(object):
+    def reset(self):
+        """Send "All Notes Off" and "Reset All Controllers" on all channels"""
+        for msg in ports.reset_messages():
+            self.send(msg)
+
+    def panic(self):
+        """Send "All Sounds Off" on all channels.
+
+        This will mute all sounding notes regardless of
+        envelopes. Useful when notes are hanging and nothing else
+        helps.
+        """
+        for msg in ports.panic_messages():
+            self.send(msg)
--- mido/backends/rtmidi.py
+++ mido/backends/rtmidi.py
@@ -7,16 +7,10 @@ import os
 import sys
 import time
 import threading
-from ..py2 import PY2
-
-if PY2:
-    import Queue as queue
-else:
-    import queue
 
 import rtmidi
 from .. import ports
-from ..parser import Parser
+from ._common import ParserQueue, PortMethods, InputMethods, OutputMethods
 
 def _get_api_lookup():
     api_to_name = {}
@@ -72,161 +66,77 @@ def get_api_names():
     return [_api_to_name[n] for n in rtmidi.get_compiled_api()]
 
 
-def _open_port(client, name, virtual=False):
-    ports = client.get_ports()
-    if len(ports) == 0:
-        raise IOError('no ports available')
+def _open_port(rt, name=None, client=None, virtual=False, api=None):
+
+    if client is not None:
+        virtual = True
+
 
     if virtual:
-        client.open_virtual_port(name)
-    else:
-        if name is None:
-            name = ports[0]
-            port_id = 0
-        elif name in ports:
-            port_id = ports.index(name)
-        else:
-            raise IOError('unknown port {!r}'.format(name))
+        if self.name is None:
+            raise IOError('virtual port must have a name')
+        
+        rt.open_virtual_port(name)
+        return name
 
-        try:
-            client.open_port(port_id)
-        except RuntimeError as err:
-            raise IOError(*err.args)
+
+    port_names = rt.get_ports()
+    if len(port_names) == 0:
+        raise IOError('no ports available')
+
+
+    if name is None:
+        name = port_names[0]
+        port_id = 0
+    elif name in port_names:
+        port_id = port_name.index(name)
+    else:
+        raise IOError('unknown port {!r}'.format(name))
+
+    
+    try:
+        rt.open_port(port_id)
+    except RuntimeError as err:
+        raise IOError(*err.args)
 
     return name
 
 
-class Port(ports.BaseIOPort):
-    """RtMidi port
+class Input(PortMethods, InputMethods):
+    def __init__(self, name=None, client=None, virtual=False,
+                 api=None, callback=None, **kwargs):
 
-    The following methods are thread safe::
+        self._queue = ParserQueue()
 
-        send()
-        reset()
-        panic()
+        self._rt = rtmidi.MidiIn()
+        self.name = _open_port(self._rt, name, client=client,
+                               virtual=virtual, api=api)
 
-        receive()
-        poll()
-        iter_pending()
-        __iter__()
+        self._rt.ignore_types(False, False, True)
 
-        @callback  (set and clear callback)
-
-    When self.callback is set it will be called every time a messages
-    comes in::
-
-        RtMidi -> self._feed_callback -> self.callback
-
-    If self.callback is not set the messages will instead be put in a
-    queue where other threads can get them::
-
-                                          Consumer threads:
-
-                                            /-> receive()
-        RtMidi -> self._feed_queue -> queue --> receive()
-                                            \-> poll()
-
-    The queue ensures that the port is thread safe and that threads
-    actually block rather than poll and wait (Python 3 only, see
-    comment inside receive()`.
-
-    send() uses a lock for thread safety.
-    """
-    # We do our own locking.
-    _locking = False
-
-    def _open(self, is_input=False, is_output=False,
-              client=None, virtual=False, api=None,
-              callback=None, **kwargs):
-
-        if client is not None:
-            virtual = True
-
-        if virtual and self.name is None:
-            raise IOError('virtual port must have a name')
-
-        if is_input is is_output is None:
-            raise IOError('port must be input or output or both')
-
-        rtapi = _get_api_id(api)
-
-        self.virtual = virtual
-        self.is_input = is_input
-        self.is_output= is_output
-
-        if client:
-            self.virtual = True
-
-        self._callback = None
-
-        self._midiin = self._midiout = None
-        input_name = output_name = None
-
-        if is_input:
-            self._midiin = rtmidi.MidiIn(name=client, rtapi=rtapi)
-            input_name = _open_port(self._midiin, self.name, self.virtual)
-
-            self._midiin.ignore_types(False, False, True)
-            self._queue = queue.Queue()
-            self._parser = Parser()
-            self.callback = callback
-
-        if is_output:
-            self._midiout = rtmidi.MidiOut(name=client, rtapi=rtapi)
-            output_name = _open_port(self._midiout, self.name, self.virtual)
-            # Turn of ignore of sysex, time and active_sensing.
-
-        # What if is_input and is_output and these differ?
-        self.name = input_name or output_name
-
-        client = self._midiin or self._midiout
-        self.api = _api_to_name[client.get_current_api()]
+        self.api = _api_to_name[self._rt.get_current_api()]
         self._device_type = 'RtMidi/{}'.format(self.api)
 
+        self._lock = threading.RLock()
 
-    def _close(self):
-        # Note: not thread safe.
-        if self.is_input:
-            self._midiin.close_port()
-            del self._midiin
+        # We need to do this last when everything is set up.
+        self.callback = callback
 
-        if self.is_output:
-            if self.autoreset:
-                self.reset()
+        self.closed = False
 
-            self._midiout.close_port()
-            del self._midiout
+    def close(self):
+        if not self.closed:
+            self._rt.close_port()
+            self.closed = True
 
-    def _send(self, msg):
-        """Send a message on the port."""
-        with self._lock:
-            self._midiout.send_message(msg.bytes())
-
-    def _receive(self, block=True):
-        # In Python 2 queue.get() doesn't respond to CTRL-C. A workaroud is
-        # to call queue.get(timeout=100) (very high timeout) in a loop, but all
-        # that does is poll with a timeout of 50 milliseconds. This results in
-        # much too high latency.
-        #
-        # It's better to do our own polling with a shorter sleep time.
-        #
-        # See Issue #49 and https://bugs.python.org/issue8844
-        if PY2:
-            sleep_time = ports.get_sleep_time()
-            while True:
-                try:
-                    return self._queue.get_nowait()
-                except queue.Empty:
-                    if block:
-                        time.sleep(sleep_time)
-                        continue
-                    else:
-                        return None
+    def receive(self, block=True):
+        if block:
+            return self._queue.get()
         else:
-            try:
-                return self._queue.get(block=block)
-            except queue.Empty:
-                return None
+            return self._queue.poll()
+
+    def poll(self):
+        return self._queue.poll()
 
     @property
     def callback(self):
@@ -234,47 +144,53 @@ class Port(ports.BaseIOPort):
 
     @callback.setter
     def callback(self, func):
-        if not self.is_input:
-            raise ValueError('not an input port')
-
         with self._lock:
-            self._midiin.cancel_callback()
+            # Make sure the callback doesn't run while were swapping it out.
+            self._rt.cancel_callback()
 
             if func:
-                # First send all queued messages to the callback.
-                while True:
-                    try:
-                        func(self._queue.get_nowait())
-                    except queue.Empty:
-                        break
+                # Make sure the callback gets all the queued messages.
+                for msg in self._queue.iterpoll():
+                    func(msg)
 
-            if func is None:
-                self._midiin.set_callback(self._feed_queue)
-                self._callback = None
-            else:
-                self._midiin.set_callback(self._feed_callback)
-                self._callback = func
+            self._callback = func
+            self._rt.set_callback(self._callback_wrapper)
 
-    def _feed_queue(self, msg_data, data):
-        self._parser.feed(msg_data[0])
-        for msg in self._parser:
-            self._queue.put(msg)
-
-    def _feed_callback(self, msg_data, data):
-        self._parser.feed(msg_data[0])
-        for msg in self._parser:
-            self._callback(msg)
+    def _callback_wrapper(self, msg_data, data):
+        self._queue.feed(msg_data[0])
+        if self._callback:
+            for msg in self._queue.iterpoll():
+                self._callback(msg)
 
 
-# A bit of trickery to keep the Backend object happy.
+class Output(PortMethods, OutputMethods):
+    def __init__(self, name=None, client=None, virtual=False,
+                 api=None, callback=None, autoreset=False, **kwargs):
 
-def Input(name=None, **kwargs):
-    return Port(name=name, is_input=True, **kwargs)
+        self.autoreset = autoreset
 
+        self._queue = ParserQueue()
 
-def Output(name=None, **kwargs):
-    return Port(name=name, is_output=True, **kwargs)
+        self._send_lock = threading.RLock()
 
+        self._rt = rtmidi.MidiOut()
+        self.name = _open_port(self._rt, name, client=client,
+                               virtual=virtual, api=api)
 
-def IOPort(name=None, **kwargs):
-    return Port(name=name, is_input=True, is_output=True, **kwargs)
+        self.api = _api_to_name[self._rt.get_current_api()]
+        self._device_type = 'RtMidi/{}'.format(self.api)
+
+        self.closed = False
+
+    def send(self, msg):
+        """Send a message on the port."""
+        with self._send_lock:
+            self._rt.send_message(msg.bytes())
+
+    def close(self):
+        if not self.closed:
+            if self.autoreset:
+                self.reset()
+
+            self._rt.close_port()
+            self.closed = True
