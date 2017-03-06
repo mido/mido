23da84e9e0bdb395d6bd607e5fa1f1b04d894650
Ole Martin Bjørndalen <ombdalen@gmail.com>
2017-03-06 15:43:14 +0100

    Added some tests for tick2second() and second2tick().
    These could be improved but a couple of test cases is better than nothing.

--- mido/midifiles/test_units.py
+++ mido/midifiles/test_units.py
@@ -1,4 +1,4 @@
-from .units import tempo2bpm, bpm2tempo
+from .units import tempo2bpm, bpm2tempo, tick2second, second2tick
 
 def test_tempo2bpm_bpm2tempo():
     for bpm, tempo in [
@@ -9,3 +9,17 @@ def test_tempo2bpm_bpm2tempo():
     ]:
         assert bpm == tempo2bpm(tempo)
         assert tempo == bpm2tempo(bpm)
+
+
+# Todo: these tests could be improved with better test values such as
+# edge cases.
+
+def test_tick2second():
+    assert tick2second(1, ticks_per_beat=100, tempo=500000) == 0.005
+    assert tick2second(2, ticks_per_beat=100, tempo=100000) == 0.002
+
+
+def test_second2tick():
+    assert second2tick(0.005, ticks_per_beat=100, tempo=500000) == 1
+    assert second2tick(0.002, ticks_per_beat=100, tempo=100000) == 2
+
