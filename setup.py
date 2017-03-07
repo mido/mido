9b71904220a9fdbc07384291b5bfb94fd9eea942
Ole Martin Bjørndalen <ombdalen@gmail.com>
2017-03-07 10:22:13 +0100

    Reinstated trove classifiers.

--- setup.py
+++ setup.py
@@ -35,4 +35,13 @@ setup(
     install_requires=[],
     license='MIT',
     zip_safe=False,
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+    ),
 )
