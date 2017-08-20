d077194cf39477146b378f8916f20328940a2a50
Sebastian Böck <sebastian.boeck@jku.at>
2017-08-20 07:36:36 +0200

    clarify docstring for tick2second() and tick2second()

--- mido/midifiles/units.py
+++ mido/midifiles/units.py
@@ -1,9 +1,9 @@
 def tick2second(tick, ticks_per_beat, tempo):
     """Convert absolute time in ticks to seconds.
 
-    Returns absolute time in seconds for a chosen MIDI file time
-    resolution (ticks per beat, also called PPQN or pulses per quarter
-    note) and tempo (microseconds per beat).
+    Returns absolute time in seconds for a chosen MIDI file time resolution
+    (ticks/pulses per quarter note, also called PPQN) and tempo (microseconds
+    per quarter note).
     """
     scale = tempo * 1e-6 / ticks_per_beat
     return tick * scale
@@ -12,9 +12,9 @@ def tick2second(tick, ticks_per_beat, tempo):
 def second2tick(second, ticks_per_beat, tempo):
     """Convert absolute time in seconds to ticks.
 
-    Returns absolute time in ticks for a chosen MIDI file time
-    resolution (ticks per beat, also called PPQN or pulses per quarter
-    note) and tempo (microseconds per beat).
+    Returns absolute time in ticks for a chosen MIDI file time resolution
+    (ticks/pulses per quarter note, also called PPQN) and tempo (microseconds
+    per quarter note). Normal rounding applies.
     """
     scale = tempo * 1e-6 / ticks_per_beat
     return int(round(second / scale))
@@ -26,7 +26,7 @@ def bpm2tempo(bpm, time_signature=(4, 4)):
 
     Depending on the chosen time signature a bar contains a different number of
     beats. These beats are multiples/fractions of a quarter note, thus the
-    returned BPM depend on the time signature.
+    returned BPM depend on the time signature. Normal rounding applies.
     """
     return int(round(60 * 1e6 / bpm * time_signature[1] / 4.))
 
