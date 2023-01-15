fc2eadede6b927f632584936a6a64da76a6797d6
Ole Martin Bjørndalen <ombdalen@gmail.com>
2023-01-15 20:01:48 +0100

    Forgot to remove a PY2 check from a test.

--- tests/messages/test_checks.py
+++ tests/messages/test_checks.py
@@ -1,16 +1,10 @@
 from pytest import raises
 from mido.messages.checks import check_time
-from mido.py2 import PY2
 
 
 def test_check_time():
     check_time(1)
     check_time(1.5)
-
-    if PY2:
-        # long should be allowed. (It doesn't exist in Python3,
-        # so there's no need to check for it here.)
-        check_time(long('9829389283L'))  # noqa: F821
 
     with raises(TypeError):
         check_time(None)
